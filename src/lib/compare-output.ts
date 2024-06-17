9e1c312c715bd97f16c7d2f90b97b2c1aaa78609
Nat-thapas <103213621+Nat-thapas@users.noreply.github.com>
2024-06-17 10:39:53 +0700

    Fix endline compare, maybe

--- src/lib/compare-output.ts
+++ src/lib/compare-output.ts
@@ -1,6 +1,6 @@
-function trimEnd(str: string, ch: string): string {
+function trimEnd(str: string, chs: string[]): string {
 	let end = str.length - 1;
-	while (end >= 0 && str[end] === ch) {
+	while (end >= 0 && chs.includes(str[end])) {
 		end--;
 	}
 	return str.slice(0, end + 1);
@@ -8,7 +8,7 @@ function trimEnd(str: string, ch: string): string {
 
 export function compareOutput(output: string, expectedOutput: string): boolean {
 	return (
-		trimEnd(output.replaceAll('\r\n', '\n').replace(/ +\n/g, '\n'), '\n') ===
-		trimEnd(expectedOutput.replaceAll('\r\n', '\n').replace(/ +\n/g, '\n'), '\n')
+		trimEnd(output.replaceAll('\r\n', '\n').replace(/ +\n/g, '\n'), ['\n', ' ']) ===
+		trimEnd(expectedOutput.replaceAll('\r\n', '\n').replace(/ +\n/g, '\n'), ['\n', ' '])
 	);
 }
