e06b15d382c7c339fdb68e6dfafe88f09c702f27
Nat-thapas <103213621+Nat-thapas@users.noreply.github.com>
2024-06-17 10:21:57 +0700

    Ignore white space before newlines on testcases result check

--- src/lib/compare-output.ts
+++ src/lib/compare-output.ts
@@ -9,6 +9,6 @@ function trimEnd(str: string, ch: string): string {
 export function compareOutput(output: string, expectedOutput: string): boolean {
 	return (
 		trimEnd(output.replaceAll('\r\n', '\n').replace(/ +\n/g, '\n'), '\n') ===
-		trimEnd(expectedOutput.replaceAll('\r\n', '\n'), '\n')
+		trimEnd(expectedOutput.replaceAll('\r\n', '\n').replace(/ +\n/g, '\n'), '\n')
 	);
 }
